840b94fb885b8f773b4ed1a17f3fdd94bb72b4c9
nhanlele <xuannhan365@gmail.com>
2023-07-05 22:11:14 +0700

    update gitignore

---
+++ CGO_Day01/CGO_Day01_Hienthiloichao/CGO_Day01_Hienthiloichao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Day01_Hienthiloichao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
