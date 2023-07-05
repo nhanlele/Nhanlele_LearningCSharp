471ef7b040f2ecb5988a14a84037a568d8056ef0
nhanlele <xuannhan365@gmail.com>
2023-07-05 22:40:59 +0700

    update BT Day01 Tinhdientich

---
+++ Day_01/CGO_Day01/CGO_Day01_Tinhdientich/CGO_Day01_Tinhdientich/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Day01_Tinhdientich
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+            Console.WriteLine("Enter width:");
+            width = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter height:");
+            height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            Console.WriteLine("Area is:" + area);
+            Console.ReadKey();
+        }
+    }
+}
