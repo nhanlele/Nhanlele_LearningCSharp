77063b74d5d40d1369b7a6465df3fe0f02c5a26e
nhanlele <xuannhan365@gmail.com>
2023-07-05 22:59:21 +0700

    Update BT Day01 Chuyendoitiente

---
+++ Day_01/CGO_Day01/CGO_Day01_Chuyendoitiente/CGO_Day01_Chuyendoitiente/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Day01_Chuyendoitiente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int tigia = 23000;
+            Console.Write("Moi ban nhap so tien:");
+            string input = Console.ReadLine();
+            int dola = int.Parse(input);
+            Console.WriteLine("So tien VND cua ban la:"  + dola*tigia);
+            Console.ReadKey();
+        }
+    }
+}
