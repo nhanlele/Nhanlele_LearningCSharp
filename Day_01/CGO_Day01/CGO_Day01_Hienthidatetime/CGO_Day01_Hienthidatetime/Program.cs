9c6ccf8f3e9b01e504fcf8adf5d00ddf918d2ea1
nhanlele <xuannhan365@gmail.com>
2023-07-05 22:36:37 +0700

    Update BT Day01_Showtimehethong

---
+++ Day_01/CGO_Day01/CGO_Day01_Hienthidatetime/CGO_Day01_Hienthidatetime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Day01_Hienthidatetime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localdate = DateTime.Now;
+            System.Console.WriteLine("Datetime now is:" + localdate);
+            Console.ReadKey();
+        }
+    }
+}
