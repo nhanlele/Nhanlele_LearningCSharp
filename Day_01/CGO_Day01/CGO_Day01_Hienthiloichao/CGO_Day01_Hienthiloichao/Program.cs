ea53526ff14510ad23b1726524a9a67b77f9a48b
nhanlele <xuannhan365@gmail.com>
2023-07-05 22:32:30 +0700

    Update BT Day01_Hienthiloichao

---
+++ Day_01/CGO_Day01/CGO_Day01_Hienthiloichao/CGO_Day01_Hienthiloichao/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Day01_Hienthiloichao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Hello: " + yourName);
+            Console.ReadKey(); 
+        }
+    }
+}
