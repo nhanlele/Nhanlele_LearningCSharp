d0fa034346cfbd70e143599d05f34f86f6866784
nhanlele <xuannhan365@gmail.com>
2023-07-05 21:34:13 +0700

    Update Bai TH 01

---
+++ CGO_Day01/Baithuchanholop_01/Baithuchanholop_01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baithuchanholop_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap ten cua ban:");
+            string sHoTen = Console.ReadLine();
+            Console.WriteLine("Ten cua ban la" + sHoTen);
+            Console.ReadKey();
+        }
+    }
+}
