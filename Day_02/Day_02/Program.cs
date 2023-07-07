0dfe667832aeff52168a9e757cf55c0c7e797621
nhanlele <xuannhan365@gmail.com>
2023-07-07 21:44:28 +0700

    Update Day_02_Giaiphuongtrinhbacnhat

---
+++ Day_02/Day_02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ngay sinh cua ban: ");
+            string input = Console.ReadLine();
+
+            DateTime  dob = DateTime.Parse(input);
+            DateTime  dob_year = new DateTime(DateTime.Now.Year, dob.Month, dob.Day);
+
+            if (dob_year > DateTime.Now)
+            {
+                Console.WriteLine("Ngay sinh nhat sap toi con " + (dob_year.Date - DateTime.Now).TotalDays);
+            }
+            else if (dob_year == DateTime.Now.Date) {
+                Console.WriteLine("Chuc Mung Sinh Nhat Ban! ");
+            }
+            else
+            {
+                dob_year = dob_year.AddYears(1);
+                Console.WriteLine("Ngay sinh nhat sap toi con " + (dob_year - DateTime.Now).TotalDays);
+            }
+            //Console.WriteLine("Ngay sinh cua ban la:" + dob.ToString());
+            Console.ReadKey();
+        }
+    }
+}
