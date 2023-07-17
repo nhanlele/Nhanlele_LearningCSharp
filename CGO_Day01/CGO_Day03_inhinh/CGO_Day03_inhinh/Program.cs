78b02da2ac8adfbe4b0e50c5d10e538676a1cdd9
nhanlele <xuannhan365@gmail.com>
2023-07-18 06:44:06 +0700

    Updata CGO_Day05

---
+++ CGO_Day01/CGO_Day03_inhinh/CGO_Day03_inhinh/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Day03_inhinh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Choice;
+            Console.Write("1.Print the rectangle ");
+            Console.Write("2.Print the square triangle ");
+            Console.Write("3.Print isosceles triangle ");
+            Console.Write("0.Exit");
+            Console.Write("Enter Your Choice: ");
+            Choice = Int32.Parse(Console.ReadLine());
+
+            switch (Choice)
+            {
+                case 1:
+                    Console.WriteLine("Print the rectangle ");
+                    Console.WriteLine("* * * * * * *");
+                    Console.WriteLine("* * * * * * *");
+                    Console.WriteLine("* * * * * * *");
+                    break;
+                
+                case 2:
+                    Console.WriteLine("Print the square triangle ");
+                    Console.WriteLine("*");
+                    Console.WriteLine("* * ");
+                    Console.WriteLine("* * * ");
+                    Console.WriteLine("* * * * ");
+                    Console.WriteLine("* * * * * ");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Print isosceles triangle ");
+                    Console.WriteLine("*");
+                    Console.WriteLine("* * ");
+                    Console.WriteLine("* * * ");
+                    Console.WriteLine("* * * * ");
+                    Console.WriteLine("* * * * * ");
+                    break;
+            }
+        }
+    }
+}
---
+++ CGO_Day01/luyentap/luyentap/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luyentap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.Write("Slpt nhap: ");
+            n = int.Parse(Console.ReadLine());
+
+            string[] arr so = new string[n];    
+            for(int i=0; i<n; i++)
+            {
+                Console.Write("Nhap gia tri {0}", i);
+                arrso[i] = Console.ReadLine();
+            }
+        }
+    }
+}
