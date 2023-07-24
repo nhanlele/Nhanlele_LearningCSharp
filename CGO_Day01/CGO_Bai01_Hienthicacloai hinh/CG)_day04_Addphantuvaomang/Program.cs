78b19f18c8b8704ec7c8a41d5040f9e506a6aac5
nhanlele <xuannhan365@gmail.com>
2023-07-24 21:58:03 +0700

    CGO Day04_Addphantuvaomang

---
+++ CGO_Day01/CGO_Bai01_Hienthicacloai hinh/CG)_day04_Addphantuvaomang/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG__day04_Addphantuvaomang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Nhap Phan Tu Thu {i}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Nhap Gia Tri Can them (X): ");
+            int X = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap Vi tri index can them (tu 0 den 9): ");
+            int index = int.Parse(Console.ReadLine());
+
+            if (index < 0 || index >= array.Length)
+            {
+                Console.WriteLine("Khong them duoc phan tu vao mang vui long chon so hop le tu 0 den 9.");
+            }
+            else
+            {
+
+                for (int i = array.Length - 1; i > index; i--)
+                {
+                    array[i] = array[i - 1];
+                }
+                array[index] = X;
+
+                Console.WriteLine("mang sau khi them:");
+                foreach (int element in array)
+                {
+                    Console.Write(element + " ");
+                }
+            }
+        }
+    }
+}
